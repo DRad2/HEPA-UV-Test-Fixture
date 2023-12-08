1adadd3647927c699ee8f93e89922b4642cbddf2
Denis Radoncic <denis.radoncic@opentrons.com>
2023-12-08 10:29:30 -0500

    Remove GUI.py

--- paramiko_test.py
+++ paramiko_test.py
@@ -1,16 +1,19 @@
 import paramiko
 import sys
+import shared_var
 from shared_var import ADC_Value
 from shared_var import command
-
+from shared_var import ssh_status
 #from shared_var import ADC_Value  # Import the shared variable
 
 ssh = paramiko.SSHClient()
 
-def establish_connection():
+def establish_connection(ip):
     try:
         # Define SSH connection parameters
-        host = "10.13.11.179"  # Replace with your BBB's IP address
+        #global ip
+        print("ip = ", ip)
+        host = ip  # Replace with your BBB's IP address
         username = "debian"  # Replace with your BBB username
         password = "temppwd"  # Replace with your BBB password
          # Initialize an SSH client
@@ -21,6 +24,11 @@ def establish_connection():
         # Connect to the BBB
         ssh.connect(host, username=username, password=password)
         print("Connected")
+        
+        global ssh_status
+        shared_var.ssh_status = "Connected"
+        print("ssh_status = ", shared_var.ssh_status)
+
 
     except paramiko.AuthenticationException:
         print("Authentication failed")
@@ -32,6 +40,7 @@ def establish_connection():
 def end_connection():
     ssh.close()  
     print("Disconnected")
+    shared_var.ssh_status = "Disconnected"
 
 def get_ADC_value(command):
     global ADC_Value
@@ -70,5 +79,85 @@ def flash_MCU():
     print(stdout.read().decode("utf-8"))
     del stdin, stdout, stderr  
 
+def test_can():
+    msg = None
+    sent = None 
+    received = None
+    
+    print("CAN test started")
+    #stdin, stdout, stderr = ssh.exec_command("source myenv/bin/activate && python can_test.py")
+    stdin, stdout, stderr = ssh.exec_command("python can_test.py")
+    print("Command sent to BBB")
+    #print(stdout.read().decode("utf-8")) #use this to send a meaningful msg
+    msg = (stdout.read().decode("utf-8"))
+    #print(msg)
 
+    lines = msg.split('\n')
+    for line in lines:
+        if "can1  5A1" in line:
+            sent = line.split("[8]")[1].strip()
+        elif "can1  00000023" in line:
+            received = line.split("[8]")[1].strip()
+
+    print("Sent: ", sent)
+    print("Received: ", received)
+
+    sent_bytes = sent.split()
+    received_bytes = received.split()
+
+    #print("sent_bytes: ", sent_bytes)
+    #print("received_bytes: ", received_bytes)
+
+    can_ok = 0
+
+    for i, (sent_byte, received_byte) in enumerate(zip(sent_bytes, received_bytes)):
+        sent_byte = sent_byte.strip("'")
+        received_byte = received_byte.strip("'")
+        if int(sent_byte, 16) + 1 == int(received_byte, 16):
+            can_ok = can_ok+1
+    
+    if can_ok == 8:
+        print ("CAN BUS OK")
+    else: print ("CAN BUS NOT OK")
+    can_ok = 0
+
+    #print("Sending the output")
+    #msg = stdout.read(30).decode("utf-8")
+    #print ("Message = ", msg)
+    del stdin, stdout, stderr
+
+def test_EEPROM():
+    msg = None
+    sent = None 
+    received = None
+    value = None
+    
+    print("EEPROM test started")
+    #stdin, stdout, stderr = ssh.exec_command("source myenv/bin/activate && python can_test.py")
+    stdin, stdout, stderr = ssh.exec_command("python EEPROM_test.py")
+    print("Command sent to BBB")
+    #print(stdout.read().decode("utf-8")) #use this to send a meaningful msg
+    msg = (stdout.read().decode("utf-8"))
+    print(msg)
+
+    lines = msg.split('\n')
+    #print(lines)
+    for line in lines[5:]:  # Consider the last two lines
+        if "can1  00000023" in line:
+            value = line.split("[8]")[1].strip()
+            if sent is None:
+                sent = value
+            elif received is None:
+                received = value
+            
+    print("Sent: ", sent)
+    print("Received: ", received)
+
+    if (sent == received):
+        print("EEPROM OK")
+
+    del stdin, stdout, stderr
         
+
+
+    
